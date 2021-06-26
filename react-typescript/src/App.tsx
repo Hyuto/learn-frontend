2621e7cc482719a9c7354d9b8ff8c5723dcc9eee
Wahyu Setianto <wahyusetianto19@gmail.com>
2021-06-27 00:20:15 +0700

    Initial commit : Init react-typescript

---
+++ react-typescript/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Learn <code>react</code> using <code>typescript</code> & <code>sass</code>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
