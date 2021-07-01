f9f5e815935905598d9192fc296b8ebf9d5dbd71
Wahyu Setianto <wahyusetianto19@gmail.com>
2021-07-01 23:29:47 +0700

    Fixing container view for react todo app

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -7,6 +7,9 @@ const App = () => {
       <header className="App-header">
         <SpinningLogo />
       </header>
+      <main>
+        <div className="box"></div>        
+      </main>
     </div>
   )
 }
---
+++ react-typescript/src/utils/getData.ts
@@ -0,0 +1,20 @@
+export const DATA:Array<Object> = [
+    {
+        'title': 'Test Todo 1',
+        'Keterangan': 'Test aja',
+        'Done': false,
+        'Date': null
+    },
+    {
+        'title': 'Test Todo 2',
+        'Keterangan': 'Test aja',
+        'Done': true,
+        'Date': null
+    },
+    {
+        'title': 'Test Todo 1',
+        'Keterangan': 'Test aja',
+        'Done': false,
+        'Date': '2021-06-11'
+    },
+]
