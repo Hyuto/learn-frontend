fcb19ac8ebd1a9c8dce74698404189f38b3aa05d
Wahyu Setianto <wahyusetianto19@gmail.com>
2021-08-10 23:51:01 +0700

    Use hardcode for decoding JWT payload and set Cookies

--- react-typescript/src/utils/tools.ts
+++ react-typescript/src/utils/tools.ts
@@ -1,10 +1,16 @@
 import axios, { AxiosInstance } from "axios";
-import jwtDecode, { JwtPayload } from "jwt-decode";
 import Cookies from "js-cookie";
 
 interface Token {
   access: string;
   refresh: string;
+}
+
+interface Payload {
+  token_type: string;
+  exp: number;
+  jti: string;
+  user_id: string;
 }
 
 const URL: string =
@@ -19,32 +25,65 @@ class API {
     refresh: "auth/login/refresh/",
     logout: "auth/logout/",
   };
-  token?: Token;
+  token: Token;
   Instance: AxiosInstance;
 
   constructor() {
-    const cookies = Cookies.get("react-todos");
-    this.verifyToken();
+    const cookies = JSON.parse(Cookies.get("react-todos") as string);
 
     if (cookies) {
+      this.verifyToken(cookies);
+    } else {
+      this.token = this.login();
     }
 
     this.Instance = this.getInstance();
   }
 
+  parseJwt = (token: string): Payload => {
+    const base64Url = token.split(".")[1];
+    const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+    const jsonPayload = decodeURIComponent(
+      atob(base64)
+        .split("")
+        .map(function (c) {
+          return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+        })
+        .join("")
+    );
+    return JSON.parse(jsonPayload);
+  };
+
   getInstance = (): AxiosInstance => {
     return axios.create({
       baseURL: URL + this.endpoint["api"],
-      headers: { Authorization: "Bearer " },
+      headers: { Authorization: `Bearer ${this.token["access"]}` },
     });
   };
 
-  verifyToken = () => {
-    const decoded = jwtDecode<JwtPayload>(
-      "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjI4MTgyMTI2LCJqdGkiOiI0NTQ0NDdjZDE1NjA0MjI5ODFlZTVlMjQ0ODUwNzNiZiIsInVzZXJfaWQiOiI5NjIyNjZkYi00YzE2LTQwZWQtYjQxOC1lNjU3Njc1ZjA0NWIifQ.KlsvZHR8Pqsq6vg-KhquugbR3hSf4T2xzd85a2DI-BA"
-    );
-    console.log(decoded);
+  refresh = (token: Token) => {};
+
+  login = (): Token => {
+    const token: Token = None;
+
+    Cookies.set("react-todos", JSON.stringify(token));
+
+    return token;
+  };
+
+  verifyToken = (token: Token) => {
+    const decoded_refresh = this.parseJwt(token["refresh"]);
+    const decoded_access = this.parseJwt(token["access"]);
+    if ((decoded_refresh.exp as number) < Math.floor(Date.now() / 1000)) {
+      console.log("login");
+    } else if ((decoded_access.exp as number) < Math.floor(Date.now() / 1000)) {
+      console.log("refresh");
+    } else {
+      return token;
+    }
   };
 }
 
 export const APIHandler: API = new API();
+
+export const Instance = APIHandler.Instance;
